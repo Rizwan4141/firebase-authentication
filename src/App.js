bbf0cee59ff477f63e2499eb76e90f34c6f4a30b
Rizwan4141 <105095961+Rizwan4141@users.noreply.github.com>
2022-05-07 11:25:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Authantication from './Components/Authantication';
+
+function App() {
+  return (
+    <div className="App">
+     <Authantication/>
+    </div>
+  );
+}
+
+export default App;
